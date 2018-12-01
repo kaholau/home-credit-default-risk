b82143358c9756521ece09361e125d6ed2b9479f
kaholau <khlaukaho@gmail.com>
2018-12-01 18:03:02 +0800

    add comment for submission

--- app/code/myutil.py
+++ app/code/myutil.py
@@ -16,12 +16,10 @@ import gc
 import warnings
 warnings.filterwarnings('ignore')
 
-def my_print(str):
-    with open('output.txt', 'a') as file:
-        file.write(str)
-    return
-
 def get_train_test_label(row=None):
+    '''
+    This function return the preprocessed data and label and kaggle test result
+    '''
     train = pd.read_csv("main_table.csv", nrows = row)
     test = pd.read_csv("test_table.csv", nrows = row)
     label = train['TARGET']
@@ -29,8 +27,10 @@ def get_train_test_label(row=None):
     print('test datasset shape:',test.shape)
     return train, test, label
 
-# undersample without replacement, return a list of undersampled df
 def undersample(df):
+    '''
+    return a list of dataframe, each df contains 50%50% neg and pos records
+    '''
     pos = df[df.TARGET == 1]
     neg = df[df.TARGET == 0]
     shuffled_neg = shuffle(neg)
@@ -46,6 +46,10 @@ def undersample(df):
     return undersampled_list
 
 def feature_select_cross_validation_undersample(feature_importance_df_, data_, test_, y_, model):
+    '''
+    Running the crossvalidation with increasing feature size in step of 50, you can find out the best features size in print out.
+    Undersampled data is used in cross validation.
+    '''
     cols_all = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(
             by="importance", ascending=False).index
     
@@ -56,12 +60,15 @@ def feature_select_cross_validation_undersample(feature_importance_df_, data_, t
         cols = cols_all[:(i*50)]
         data_sf = data_[cols]
         data_sf[['TARGET','Unnamed: 0']] = data_[['TARGET','Unnamed: 0']]
-#         print(i," selected index:",cols)
         print(i)
         oof_preds, test_preds, importances, folds = cross_validation_undersample(data_sf, test_, y_, model)
         test_preds.to_csv('{}_{}_submission_undersample.csv'.format(str(model),i), index=False)
 
 def feature_select_cross_validation(feature_importance_df_, data_, test_, y_, model):
+    '''
+    Running the crossvalidation with increasing feature size in step of 50, you can find out the best features size in print out.
+    Entire dataset is used in cross validation.
+    '''
     cols_all = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(
             by="importance", ascending=False).index
     
@@ -72,7 +79,6 @@ def feature_select_cross_validation(feature_importance_df_, data_, test_, y_, mo
         cols = cols_all[:(i*50)]
         data_sf = data_[cols]
         data_sf[['TARGET','Unnamed: 0']] = data_[['TARGET','Unnamed: 0']]
-#         print(i," selected index:",cols)
         print(i)
         my_print(i)
         oof_preds, test_preds, importances, folds = cross_validation(data_sf, test_, y_, model)
@@ -80,14 +86,19 @@ def feature_select_cross_validation(feature_importance_df_, data_, test_, y_, mo
         
         
 def cross_validation_undersample(data_, test_, y_, model):
+    
     from datetime import datetime
     start_time = datetime.now()    
+    
     print('start at: {}'.format(start_time))
+    
+    # collecting the validation prediction in each fold
     oof_preds = np.zeros(data_.shape[0])
+    # collecting the test set prediction in each fold
     sub_preds = np.zeros(test_.shape[0])
     
     udf = undersample(data_)
-    folds_ = KFold(n_splits=len(udf), shuffle=True, random_state=546789)
+    
     feature_importance_df = pd.DataFrame()
     
     feats = [f for f in data_.columns if f not in ['SK_ID_CURR','TARGET','Unnamed: 0']]
@@ -105,9 +116,6 @@ def cross_validation_undersample(data_, test_, y_, model):
         trn_y = label.iloc[trn_idx]
         val_x = u[feats].iloc[val_idx]
         val_y = label.iloc[val_idx]
-        #print(trn_x)
-        #print('train_x shape:', trn_x)
-        #print('val_x shape:', val_x.shape)
            
          # Make the new model 
         model.init_model()
@@ -115,43 +123,48 @@ def cross_validation_undersample(data_, test_, y_, model):
         # Train on the training data
         model.train(trn_x, trn_y)
 
-        # Make predictions
-        # Make sure to select the second column only    
-        #print('oof_preds has shape:', oof_preds[val_idx].shape)
-        #'Unnamed: 0' is same as the idx in the original data
+        # Make predictions        
+        #collecting the probability of prediction
         true_val_idx = u['Unnamed: 0'].iloc[val_idx].values.flat
         oof_preds[true_val_idx] = model.predict(val_x)
+        
+        #average the prediction of test set
         sub_preds +=  model.predict(test_[feats])/ len(udf)
         
         fold_importance_df = pd.DataFrame()
         fold_importance_df["feature"] = feats
         
-        #print(fold_importance_df.shape, log_reg.coef_.shape)
-        
+        #collect the importance for computing the average importance
         fold_importance_df["importance"] = model.get_coef()
         fold_importance_df["fold"] = n_fold + 1
         feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
-        #print(val_y.shape, oof_preds[val_idx].shape)
-        #print(val_y, oof_preds[val_idx])
+        
         print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(val_y, oof_preds[val_idx])))
+        
+        #del dataframe to release memory
         del trn_x, trn_y, val_x, val_y
         gc.collect()
         
     print('Full AUC score %.6f' % roc_auc_score(y_, oof_preds)) 
     
     test_['TARGET'] = sub_preds
-    #test_[['SK_ID_CURR', 'TARGET']].to_csv('{}_submission_undersample.csv'.format(model), index=False, float_format='%.8f')
+    
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
+    #this folds_ is useless, just convenient to call report() which is shared with cross_validation()
+    folds_ = KFold(n_splits=5, shuffle=True, random_state=546789)
     return oof_preds, test_[['SK_ID_CURR', 'TARGET']], feature_importance_df, folds_
 
 def cross_validation(data_, test_, y_, model):
     from datetime import datetime
     start_time = datetime.now()
     print('start at: {}'.format(start_time))
+    #5-fold
     folds_ = KFold(n_splits=5, shuffle=True, random_state=546789)
     
+    # collecting the validation prediction in each fold
     oof_preds = np.zeros(data_.shape[0])
+    # collecting the test set prediction in each fold
     sub_preds = np.zeros(test_.shape[0])
     
     feature_importance_df = pd.DataFrame()
@@ -159,6 +172,7 @@ def cross_validation(data_, test_, y_, model):
     feats = [f for f in data_.columns if f not in ['SK_ID_CURR','TARGET','Unnamed: 0']]
     
     for n_fold, (trn_idx, val_idx) in enumerate(folds_.split(data_)):
+        # prepare index for cross validation
         trn_x, trn_y = data_[feats].iloc[trn_idx], y_.iloc[trn_idx]
         val_x, val_y = data_[feats].iloc[val_idx], y_.iloc[val_idx]
         
@@ -169,49 +183,52 @@ def cross_validation(data_, test_, y_, model):
         model.train(trn_x, trn_y)
 
         # Make predictions
-        # Make sure to select the second column only    
-        #print('oof_preds has shape:', oof_preds[val_idx].shape)
+        #collecting the probability of prediction
         oof_preds[val_idx] = model.predict(val_x)
+        
+        #average the prediction of test set
         sub_preds +=  model.predict(test_[feats])/ folds_.n_splits
         
         fold_importance_df = pd.DataFrame()
         fold_importance_df["feature"] = feats
         
-        #print(fold_importance_df.shape, log_reg.coef_.shape)
-        
+        #collect the importance for computing the average importance
         fold_importance_df["importance"] = model.get_coef()
         fold_importance_df["fold"] = n_fold + 1
         feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
-        #print(val_y.shape, oof_preds[val_idx].shape)
-        #print(val_y, oof_preds[val_idx])
+        
         result_str = 'Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(val_y, oof_preds[val_idx]))
         print(result_str)
-        my_print(result_str)
+        #del dataframe to release memory
         del trn_x, trn_y, val_x, val_y
         gc.collect()
+        
     result_str = 'Full AUC score %.6f' % roc_auc_score(y_, oof_preds)
     print(result_str) 
-    my_print(result_str)
-    
+        
     test_['TARGET'] = sub_preds
-    #test_[['SK_ID_CURR', 'TARGET']].to_csv('{}_submission_undersample.csv'.format(model), index=False, float_format='%.8f')
     
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
     return oof_preds, test_[['SK_ID_CURR', 'TARGET']], feature_importance_df, folds_
 
 def display_importances(feature_importance_df_,title):
+    '''
+    plot the bar chart of feature importance
+    '''
+    #average the importance obtained from cross validation and sort and select the top 50
     cols = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(
         by="importance", ascending=False)[:50].reset_index()
     plt.figure(figsize=(8,10))
-    sns.barplot(x="importance", y="feature", 
-                data=cols)
+    sns.barplot(x="importance", y="feature", data=cols)
     plt.title('{} Features '.format(title))
     plt.tight_layout()
     plt.savefig('{}_importances.png'.format(title))
 
 def display_roc_curve(y_, oof_preds_, folds_idx_,title, is_with_nfold):
-    # Plot ROC curves
+    '''
+    Plot ROC curves
+    '''
     plt.figure(figsize=(6,6))
     scores = [] 
     if(is_with_nfold):
@@ -240,7 +257,7 @@ def display_roc_curve(y_, oof_preds_, folds_idx_,title, is_with_nfold):
     plt.savefig('{}_roc_curve.png'.format(title))
     
 def display_precision_recall(y_, oof_preds_, folds_idx_, title, is_with_nfold):
-    # Plot ROC curves
+    # Plot precision and recall curves
     plt.figure(figsize=(6,6))
     
     scores = [] 
@@ -267,22 +284,37 @@ def display_precision_recall(y_, oof_preds_, folds_idx_, title, is_with_nfold):
     plt.tight_layout()
     
     plt.savefig('{}_recall_precision_curve.png'.format(title))
-    
+
 def report(test_preds, folds, importances, data, y, oof_preds, title, is_with_nfold):
+    '''
+    one function to call graph ploting for importance and roc curve
+    '''
     print(title)
     test_preds.to_csv('{}_submission.csv'.format(title), index=False)
-    # Display a few graphs
+    
     folds_idx = [(trn_idx, val_idx) for trn_idx, val_idx in folds.split(data)]
     display_importances(importances, title)
     display_roc_curve(y, oof_preds, folds_idx, title, is_with_nfold)
     display_precision_recall(y, oof_preds, folds_idx, title, is_with_nfold)
     
 def opt(features, data_):
+    '''
+    calling bayesian optimization
+    '''
+    #init random
     random.seed(time.clock())
+    
     def bayes_parameter_opt_lgb(data, init_round, opt_round, n_estimators, learning_rate, output_process=False):
-        
+        '''
+        The calling function for bayesian optimisation for lightGBM only
+        '''
         # parameters
         def lgb_eval(num_leaves, feature_fraction, bagging_fraction, max_depth, lambda_l1, lambda_l2, min_split_gain, min_child_weight,bagging_freq):
+            '''
+            evaluation function( callback function) during optimization
+            '''
+            
+            #pack the tuning value into a dictionary
             params = {'application':'binary','num_iterations': n_estimators, 'learning_rate':learning_rate, 'early_stopping_round':100, 'metric':'auc'}
             params["num_leaves"] = int(round(num_leaves))
             params['feature_fraction'] = max(min(feature_fraction, 1), 0)
@@ -293,33 +325,32 @@ def opt(features, data_):
             params['min_split_gain'] = min_split_gain
             params['min_child_weight'] = min_child_weight
             params['bagging_freq'] = int(round(bagging_freq))
-            us_df = data
-#             us_df = undersample(data)
-#             idx1 = random.randint(0,len(us_df)-1)
-#             us_df1 = us_df[idx1]
-#             idx2 = random.randint(0,len(us_df)-1)
-#             while idx1 == idx2:
-#                 idx2 = random.randint(0,len(us_df)-1)
-#             us_df2 = us_df[idx2]
-#             us_df = pd.concat([us_df1, us_df2])
-            
-            #print('us_df.shape: ',us_df.shape)
-            #categorical_feats = us_df.columns[input_df.dtypes == 'object']
+
+            #manully change the condition
+            if False:
+                #using entire dataset 
+                us_df = data
+            else:
+                #using the undersampled data
+                us_df = undersample(data)
+                us_df = us_df[random.randint(0,len(us_df)-1)]
+
             train_data = lgb.Dataset(data=us_df[features], label=us_df['TARGET'], free_raw_data=False)
             random_seed = random.randint(1,100)
         
+            #pass the param and training data into lightgbm evaluation function
             cv_result = lgb.cv(params, train_data, nfold=3, seed=random_seed, stratified=True, verbose_eval =200, metrics=['auc'])
             return max(cv_result['auc-mean'])
-        # range 
-#         {'boosting_type': 'gbdt', 'class_weight': None, 'colsample_bytree': 1.0, 'importance_type': 'split', 'learning_rate': 0.1, 'max_depth': -1, 'min_child_samples': 20, 'min_child_weight': 0.001, 'min_split_gain': 0.0, 'n_estimators': 100, 'n_jobs': -1, 'num_leaves': 31, 'objective': None, 'random_state': None, 'reg_alpha': 0.0, 'reg_lambda': 0.0, 'silent': True, 'subsample': 1.0, 'subsample_for_bin': 200000, 'subsample_freq': 0}
-        lgbBO = BayesianOptimization(lgb_eval, {'num_leaves': (23, 39),
+        
+        # provide the evaluation function and param tunning range
+        lgbBO = BayesianOptimization(lgb_eval, {'num_leaves': (10, 80),
                                                 'feature_fraction': (0.1, 0.9),
                                                 'bagging_fraction': (0.8, 1),
-                                                'bagging_freq':(1,10),
-                                                'max_depth': (5, 8.99),
-                                                'lambda_l1': (0, 5),
-                                                'lambda_l2': (0, 3),
-                                                'min_split_gain': (0.001, 0.1),
+                                                'bagging_freq':(1,20),
+                                                'max_depth': (3, 8.99),
+                                                'lambda_l1': (0, 10),
+                                                'lambda_l2': (0, 10),
+                                                'min_split_gain': (0.001, 0.5),
                                                 'min_child_weight': (5, 50)}, random_state=0)
         # optimize
         lgbBO.maximize(init_points=init_round, n_iter=opt_round)
@@ -329,9 +360,11 @@ def opt(features, data_):
 
         # return best parameters
         return lgbBO
-
+    
+    #optimization call, we like to tune the parameters in this document
     opt_params = bayes_parameter_opt_lgb(data_, init_round=5, opt_round=10, n_estimators=10000, learning_rate=0.05, output_process=True)
-
+    
+    return opt_params
 
 
 
